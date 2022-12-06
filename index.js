bfe6a750036653cacfc1b314910e872e524d6dfb
zoeanderson <zvann813@gmail.com>
2022-12-05 20:54:47 -0700

    commit tree 3d5770d40a4dbe5a5823dd75d55904172cbb99a8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+let names = ["Guadalupe", "Ollie", "Aki"]
+let event = 'suprise'
+
+function writeCards(names, event){
+   let cardArray = []
+    for (let i = 0; i < names.length; i++) {
+        cardArray.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    
+    }
+    return cardArray;
+}
+
+function countDown(number){
+    let countDown = 0;
+while (number >= 0) {
+  console.log(number--);
+}
+}
